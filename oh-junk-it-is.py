efa974b3a1063fcb6c77805061d83873f490d359
Joseph Cafarelli <64694539+powerman2824@users.noreply.github.com>
2022-12-13 04:53:29 +0000

    done for now

--- oh-junk-it-is.py
+++ oh-junk-it-is.py
@@ -1,6 +1,17 @@
 x = int(input())
 y = 0
+a = []
+c = []
+
 while y != x:
     z = int(input())
     y = y + 1
-    print(z)
+    c.append(z)
+        
+b = int(input())
+
+d = []
+for i in c:
+    if i <= b:
+        d.append(i)
+print(','.join(str(i) for i in d), end=',')
